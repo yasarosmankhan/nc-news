6c3f12e50bc0de6f09401e4aa1585c3c4a55cdcd
yasarosmankhan <yasar_osman@live.co.uk>
2023-10-05 10:08:38 +0100

    config changes

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+	path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+	throw new Error('PGDATABASE not set');
 }
 
 module.exports = new Pool();
